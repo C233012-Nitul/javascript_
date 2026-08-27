3739cb92f5a34b5cbe4db59dc5cd4d9091132edf
NITUL <mohammadnitul@gmail.com>
2026-08-28 00:08:26 +0600

    c3_3

---
+++ check.js
@@ -0,0 +1,10 @@
+// import add from "./export.js"
+// import {sub} from "./export.js"
+
+import add, {sub, pi} from "./export.js"
+//import {sub} from "./export.js"
+
+
+console.log(add(5, 3))
+
+console.log(sub(8, 9), pi)
---
+++ export.js
@@ -0,0 +1,13 @@
+//export import
+
+function add(a, b){
+    return a + b
+}
+
+export function sub(a, b) {
+    return a > b ? a - b : b - a
+}
+export const pi = 3.1416
+
+
+export default add;
