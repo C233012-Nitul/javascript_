f6ef5959c0b5469d4a4b4a51a98715df68073b67
NITUL <mohammadnitul@gmail.com>
2026-08-29 22:20:36 +0600

    C4_1

---
+++ todo/app.js
@@ -0,0 +1,12 @@
+console.log("connected");
+
+let taskInput = document.getElementById("taskInput");
+let addBtn = document.getElementById("addBtn");
+let taskList = document.getElementById("taskList");
+// console.log(taskInput, addBtn, taskInput)
+
+// let addBtn2 = document.querySelector("button")
+//console.log(addBtn2)
+
+// let addBtn2 = document.querySelector("#addBtn")
+//console.log(addBtn2)
