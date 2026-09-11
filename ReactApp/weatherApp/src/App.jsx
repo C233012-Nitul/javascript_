b892a9bb96ababc72cf1a51b1af87d0951bc7154
NITUL <mohammadnitul@gmail.com>
2026-09-11 12:09:42 +0600

    weaterApp

---
+++ ReactApp/weatherApp/src/App.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/about",
+    element: <About/>,
+  },
+]);
+
+function Router() {
+  return <RouterProvider router={router} />;
+}
+
+export default Router;
---
+++ ReactApp/weatherApp/src/pages/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <p>About US From Page</p>
+    </div>
+  );
+}
